654c1d58220dbc9ef51b9eec6c71c3e9f069b3e7
1MrHous1 <maciejlaskawski@gmail.com>
2020-10-26 13:26:53 +0100

    module 1 of 3

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+// const test = document.getElementsByClassName('text').innerHTML = '<ol><li>html data</li></ol>';
+
+const elem = document.createElement('div');
+const elemText = document.createTextNode('fck u');
